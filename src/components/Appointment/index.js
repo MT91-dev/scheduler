1e5303788588361c908b15d3e3239317d44b78ec
Mohammad Tariq <tariqm1991@gmail.com>
2022-08-10 17:36:16 -0400

    Update form with show/empty components by passing state/props and update story.

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,8 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, Fragment } from "react";
+import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
 import "components/Appointment/styles.scss";
 
 export default function Appointment(props) {
   return (
-    <article className="appointment"></article>
+    <article className="appointment">
+      <Header time={props.time} />
+      {props.interview ?
+        (<Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+          onDelete={props.onDelete}
+          onEdit={props.onEdit} />)
+        :
+        (<Empty onAdd={props.onAdd} />)}
+    </article>
   );
 }
