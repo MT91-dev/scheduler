df9fbbad2d1cca299d4e0a419b3ed9c6dce9bdd1
MT91-dev <tariqm1991@gmail.com>
2022-08-11 19:26:06 +0000

    Import useState/axios and fetch days data with API

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,25 +1,8 @@
-import React, { useState } from "react";
+import React, { Fragment, useState, useEffect } from "react";
+import axios from "axios";
 import DayList from "./DayList";
 import Appointment from "components/Appointment";
 import "components/Application.scss";
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 const appointments = {
   "1": {
@@ -62,6 +45,15 @@ const appointments = {
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    axios.get("/api/days").then(res => {
+      setDays(res.data);
+    }).catch(err => {
+      console.log(err);
+    })
+  });
 
   const allAppointments = Object.values(appointments).map((appointment) => {
     return (
@@ -72,7 +64,6 @@ export default function Application(props) {
 
     )
   })
-
 
   return (
     <main className="layout">
