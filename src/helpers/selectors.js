3aeaaca357ecdc51e9fb3d0af3bd5bb7fb8d99fa
MT91-dev <tariqm1991@gmail.com>
2022-08-11 19:49:20 +0000

    Create helper function for days using selectors and apply filter method to find appointments for given day

---
+++ src/helpers/selectors.js
@@ -0,0 +1,15 @@
+export function getAppointmentsForDay(state, day) {
+  const filteredDays = state.days.filter(currentDay => currentDay.name === day);
+  if (state.days.length === 0 || filteredDays.length === 0) {
+    return [];
+  }
+
+  const appointmentsFromDays = filteredDays[0].appointments;
+
+  let filteredAppointments = [];
+
+  for (let appointment of appointmentsFromDays) {
+    filteredAppointments.push(state.appointments[appointment]);
+  }
+  return filteredAppointments;
+}
