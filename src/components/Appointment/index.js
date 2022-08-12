486438f53b129c0d92926d4fb034dc3d145ead74
MT91-dev <tariqm1991@gmail.com>
2022-08-12 21:25:36 +0000

    Update appointment index.js to use modes for Create, Empty & Form with conditional rendering

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,20 +2,41 @@ import React, { useState, Fragment } from "react";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
+import Form from "components/Appointment/Form";
+import useVisualMode from "hooks/useVisualMode";
 import "components/Appointment/styles.scss";
 
 export default function Appointment(props) {
+
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {props.interview ?
-        (<Show
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
+        <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
           onDelete={props.onDelete}
-          onEdit={props.onEdit} />)
-        :
-        (<Empty onAdd={props.onAdd} />)}
+          onEdit={props.onEdit}
+        />
+      )}
+      {mode === CREATE && (
+        <Form
+          name={props.name}
+          value={props.value}
+          interviewers={[]}
+          onSave={props.onSave}
+          onCancel={() => back(EMPTY)}
+        />
+      )}
     </article>
   );
 }
