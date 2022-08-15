ba82090cd7d78c9f74d0363e095d20a3581ce243
MT91-dev <tariqm1991@gmail.com>
2022-08-15 20:49:32 +0000

    Add book/cancel interview to application.js & save/remove functions to appointment index.js

--- src/components/Application.js
+++ src/components/Application.js
@@ -16,6 +16,57 @@ export default function Application(props) {
   const setDay = day => setState({ ...state, day });
   const setDays = days => setState(prev => ({ ...prev, days }));
 
+
+  //function that submits axios put request to update database
+  const bookInterview = (id, interview) => {
+
+    const appointment = {
+      ...state.appointments[id],
+      interview: { ...interview }
+    };
+
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+
+    const putRequestAPIURL = `http://localhost:8001/api/appointments/${id}`;
+
+    let requestData = {
+      putRequestAPIURL,
+      data: appointment
+    };
+
+    return axios.put(putRequestAPIURL, appointment).then(() => {
+      setState({ ...state, appointments })
+    })
+  }
+
+  //function that submits axios delete request to update database
+  const cancelInterview = (id) => {
+
+    const appointment = {
+      ...state.appointments[id],
+      interview: null
+    };
+
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+
+    const deleteRequestAPIURL = `http://localhost:8001/api/appointments/${id}`;
+
+    let requestData = {
+      deleteRequestAPIURL,
+      data: appointment
+    };
+
+    return axios.delete(deleteRequestAPIURL, appointment).then(() => {
+      setState({ ...state, appointments})
+    })
+  }
+
   useEffect(() => {
     Promise.all([
       axios.get('http://localhost:8001/api/days'),
@@ -32,7 +83,6 @@ export default function Application(props) {
     const interview = getInterview(state, appointment.interview);
     const interviewers = getInterviewersForDay(state, state.day);
 
-
     return (
       <Appointment
         key={appointment.id}
@@ -40,6 +90,8 @@ export default function Application(props) {
         time={appointment.time}
         interview={interview}
         interviewers={interviewers}
+        bookInterview={bookInterview}
+        cancelInterview={cancelInterview}
       />
     )
   })
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,6 +3,9 @@ import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
 import Form from "components/Appointment/Form";
+import Status from "components/Appointment/Status";
+import Error from "components/Appointment/Error";
+import Confirm from "components/Appointment/Confirm";
 import useVisualMode from "hooks/useVisualMode";
 import "components/Appointment/styles.scss";
 
@@ -11,10 +14,36 @@ export default function Appointment(props) {
   const EMPTY = "EMPTY";
   const SHOW = "SHOW";
   const CREATE = "CREATE";
+  const SAVING = "SAVING";
+  const DELETING = "DELETING";
+  const CONFIRM = "CONFIRM";
+  const EDIT = "EDIT";
 
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
   );
+
+
+  //function that saves the interview to the database
+  function save(name, interviewer) {
+    const interview = {
+      student: name,
+      interviewer
+    }
+
+    transition(SAVING);
+
+    props.bookInterview(props.id, interview)
+      .then(() => { transition(SHOW) })
+  }
+
+  //function that deletes the interview from the database
+  function remove() {
+    transition(DELETING);
+
+    props.cancelInterview(props.id)
+      .then(() => { transition(EMPTY) })
+  }
 
   return (
     <article className="appointment">
@@ -24,17 +53,39 @@ export default function Appointment(props) {
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
-          onDelete={props.onDelete}
-          onEdit={props.onEdit}
+          onDelete={() => transition(CONFIRM)}
+          onEdit={() => transition(EDIT)}
         />
       )}
       {mode === CREATE && (
         <Form
-          // name={props.name}
-          // value={props.value}
+          name={props.name}
+          value={props.value}
           interviewers={props.interviewers}
-          onSave={props.onSave}
+          onSave={save}
           onCancel={() => back(EMPTY)}
+        />
+      )}
+      {mode === SAVING && (
+        <Status message="Saving" />
+      )}
+      {mode === DELETING && (
+        <Status message="Deleting" />
+      )}
+      {mode === CONFIRM && (
+        <Confirm
+          onConfirm={remove}
+          onCancel={back}
+          message="Are you sure you would like to delete the appointment?"
+        />
+      )}
+      {mode === EDIT && (
+        <Form
+          student={props.interview.student}
+          interviewer={props.interview.interviewer.id}
+          interviewers={props.interviewers}
+          onSave={save}
+          onCancel={back}
         />
       )}
     </article>
